d836a0a7775a4abc9fffd23649057e140fe84c73
Khushi-Magnani445 <khushimagnani9106@gmail.com>
2024-11-23 16:52:14 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4CAF50",
+        secondary: "#FFC107",
+      },
+    },
+  },
+
+  plugins: [],
+};
